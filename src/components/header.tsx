13ad283f0210c2abc3bf9539f86fe43b776c69cb
zyreoo <simone.marton89@gmail.com>
2024-10-02 15:36:39 +0300

    make progress on the header section

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,12 +4,15 @@ export default function Home() {
     return (
         <div className={styles.header}>
             <div className={styles.logoContainer}>
-                <img src="logo." alt="logo" className={styles.logo} />
+                <img src="logo.png" alt="logo" className={styles.logoContainer} />
+            </div>
+            <div>
+                <h1 className={styles.headerTitle}>Cazari Praid</h1>
             </div>
             <div className={styles.itemContainer}>
                 <a className={styles.headerItem}>Pensiune</a>
                 <a className={styles.headerItem}>Hotel</a>
-                <a className={styles.headerItem}>Cabin</a>
+                <a className={styles.headerItem}>Cabana</a>
                 <a className={styles.headerItem}>Meniu</a>
             </div>
         </div>
