e1c2cba1f956e9f2066aa4125161a2a4e058174e
zyreoo <simone.marton89@gmail.com>
2024-10-06 21:46:15 +0300

    make progress on the footer section

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,9 +8,12 @@ export default function Home() {
             <div className={styles.logoContainerFooter}>
                 <img src="logo.jpeg" alt="logo" className={styles.photoContainerFooter} />
             </div>
-            <h1>Contact</h1>
-            <p>t is a long established fact that</p>
-            <p><a href="mailto:">contact@cazaripraid.ro</a></p>
+            <div className={styles.contactContainer}>
+            <h1 className={styles.contactTitle}>Contact</h1>
+            <p className={styles.contactItem}>t is a long established fact that</p>
+            <a href="mailto:" className={styles.contactItem}>contact@cazaripraid.ro</a>
+            </div>
+
         </div>
     );
 }
