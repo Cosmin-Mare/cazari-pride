4a3abf5f5552c7757b20610b9a49241648485c46
zyreoo <simone.marton89@gmail.com>
2024-10-07 21:55:31 +0300

    make progress on the card container

---
+++ src/components/card.tsx
@@ -0,0 +1,11 @@
+import styles from "./card.module.css"
+
+
+
+export default function Home() {
+    return (
+        <div className={styles.cardsContainer}>
+            
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,14 @@ import { Gallery, Item } from "react-photoswipe-gallery";
 import Header from "../components/header";
 import Carousel from "@/components/carousel";
 import Footer from '../components/footer';
+import Card from '../components/card'
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Carousel />
+      <Card />
       <Footer />
     </div>
   );
