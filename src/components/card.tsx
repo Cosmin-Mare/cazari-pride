877429abf87d5804d3d72e2fb0c0dbe1fca310a8
zyreoo <simone.marton89@gmail.com>
2024-10-08 16:51:26 +0300

    make progress on the card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,8 +4,17 @@ import styles from "./card.module.css"
 
 export default function Home() {
     return (
-        <div className={styles.cardsContainer}>
-            
+        <div className={styles.allCardsContainer}>
+            <div className={styles.cardContainer}>
+                <div className={styles.photoCard}>
+                    <img src='/carousel/carousel-1.png' />
+                </div>
+             <div className={styles.itemContainer}>
+                <h2> </h2>
+                <a></a>
+                <a></a>
+             </div>
+            </div>
         </div>
     );
 }
