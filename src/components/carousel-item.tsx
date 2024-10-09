5d7b05cfeffea567e742f383cdc4e9f385e3de5f
Cosmin-Mare <cosmintudor.mare@cnamd.onmicrosoft.com>
2024-10-09 15:43:53 +0300

    finish mvp of carousel

--- src/components/carousel-item.tsx
+++ src/components/carousel-item.tsx
@@ -1,14 +1,14 @@
 import styles from './carousel-item.module.css';
 
-interface CarouselItemProps {
+export interface CarouselItemProps {
     num: number;
     link: string;
-    active?: boolean;
+    position?: "left" | "right" | "center" | "none"
 }
 
 export default function CarouselItem(props: CarouselItemProps) {
     return (
-    <div className={styles.carouselItem}>
+    <div className={`${styles.carouselItem} ${props.position === "left" ? styles.left : props.position === "right" ? styles.right : props.position === "center" ? styles.center : styles.none}`}>
         <a>
           <img src={`/carousel/carousel-${props.num}.png`}/>
         </a>
--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,40 +1,58 @@
 import { use, useEffect, useState } from "react";
 import styles from "./carousel.module.css";
-import CarouselItem from "./carousel-item";
+import CarouselItem, { CarouselItemProps } from "./carousel-item";
 
 export default function Carousel() {
-  const [carouselItems, setCarouselItems] = useState<JSX.Element[]>([])
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [carouselItems, setCarouselItems] = useState<CarouselItemProps[]>([
+    {num: 0, link: "#"},
+    {num: 1, link: "#"},
+    {num: 2, link: "#"},
+    {num: 3, link: "#"},
+    {num: 4, link: "#"}
+  ])
+  const [slideInterval, setSlideInterval] = useState<NodeJS.Timeout>()
   useEffect(() => {
-    setCarouselItems([
-      <CarouselItem num={1} link="#" active />,
-      <CarouselItem num={2} link="#" />,
-      <CarouselItem num={3} link="#" />,
-      <CarouselItem num={4} link="#" />,
-      <CarouselItem num={5} link="#" />
-    ])
-    setInterval(() => {
-      carouselItems.forEach((item, index) => {
-        if(item.props.active){
-          item.props.active = false;
-          if(index == carouselItems.length-1)
-            carouselItems[0].props.active = true;
-          else
-            carouselItems[index+1].props.active = true
-        }
-      })
-    }, 2000)
-  })
+    setSlideInterval(setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % carouselItems.length);
+    }, 3000))
+  }, [])
+  useEffect(() => {
+    console.log(currentSlide)
+  }, [currentSlide])
   return (
-    <div>
       <div className={styles.carousel}>
+        <button onClick={() => {
+          clearInterval(slideInterval)
+          setCurrentSlide((prevSlide) => (prevSlide == 0 ? 4 : prevSlide-1));
+          setSlideInterval(setInterval(() => {
+            setCurrentSlide((prevSlide) => (prevSlide + 1) % carouselItems.length);
+          }, 3000))
+        }} className={styles.leftArrow}>
+          <div className={styles.arrow}>
+            <p>{"<"}</p>
+          </div>
+        </button>
         <div className={styles.carouselContainer}>
-          <CarouselItem num={1} link="#" active />
-          <CarouselItem num={2} link="#" />
-          <CarouselItem num={3} link="#" />
-          <CarouselItem num={4} link="#" />
-          <CarouselItem num={5} link="#" />
+          {currentSlide === 0 ? <CarouselItem {...carouselItems[carouselItems.length-1]} position="left" /> :
+          <CarouselItem {...carouselItems[currentSlide-1]} position="left"/>}
+          <CarouselItem {...carouselItems[currentSlide]} position="center"/>
+          {currentSlide === carouselItems.length-1 ? <CarouselItem {...carouselItems[0]} position="right"/> : 
+          <CarouselItem {...carouselItems[currentSlide+1]} position="right" />
+          }
         </div>
+        <button onClick={() => {
+          clearInterval(slideInterval)
+          setCurrentSlide((prevSlide) => (prevSlide + 1) % carouselItems.length);
+          setSlideInterval(setInterval(() => {
+            setCurrentSlide((prevSlide) => (prevSlide + 1) % carouselItems.length);
+          }, 3000))
+        }} className={styles.rightArrow}>
+          <div className={styles.arrow}>
+            <p>{">"}</p>
+          </div>
+        </button>
       </div>
-    </div>
+      
   );
 }
